1ce188fb59b8fdaa5e2a1c95e8cddd8108b2586d
Stephen Moore <judo2000@comcast.net>
2022-06-23 20:44:43 -0600

    Install react-bootstrap and bootstrap swatch along with a navbar in the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -5,7 +6,11 @@ const App = () => {
   return (
     <>
       <Header />
-      <h1>Welcome to ProShop</h1>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
       <Footer />
     </>
   );
