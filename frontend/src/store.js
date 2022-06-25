a305ee8fe3b297eaabd99c7de39b15b9b2543427
Stephen Moore <judo2000@comcast.net>
2022-06-25 08:10:46 -0600

    Install redux and redux toolkit

---
+++ frontend/src/store.js
@@ -0,0 +1,38 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   productListReducer,
+//   productDetailsReducer,
+// } from './reducers/productReducers';
+// import { cartReducer } from './reducers/cartReducers';
+// import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
+
+const reducer = {
+  // productList: productListReducer,
+  // productDetails: productDetailsReducer,
+  // cart: cartReducer,
+  // userLogin: userLoginReducer,
+  // userRegister: userRegisterReducer,
+};
+
+// get cart Items from local storage
+// const cartItemsFromStorage = localStorage.getItem('cartItems')
+//   ? JSON.parse(localStorage.getItem('cartItems'))
+//   : [];
+
+// get userInfo from local storage
+// const userInfoFromStorage = localStorage.getItem('userInfo')
+//   ? JSON.parse(localStorage.getItem('userInfo'))
+//   : null;
+
+const preloadedState = {
+  // cart: { cartItems: cartItemsFromStorage },
+  // userLogin: { userInfo: userInfoFromStorage }
+};
+
+const store = configureStore({
+  reducer,
+  preloadedState,
+  devTools: process.env.NODE_ENV !== 'production', //only show devTools when in production
+});
+
+export default store;
