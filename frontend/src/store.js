7c8e7959ff31ca4bcbc93ae512a50b1ff1f4a6db
Stephen Moore <judo2000@comcast.net>
2022-06-25 12:34:14 -0600

    Create cart actions and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,35 +3,35 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
-// import { cartReducer } from './reducers/cartReducers';
+import { cartReducer } from './reducers/cartReducers';
 // import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = {
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  // cart: cartReducer,
+  cart: cartReducer,
   // userLogin: userLoginReducer,
   // userRegister: userRegisterReducer,
 };
 
 // get cart Items from local storage
-// const cartItemsFromStorage = localStorage.getItem('cartItems')
-//   ? JSON.parse(localStorage.getItem('cartItems'))
-//   : [];
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartitems'))
+  : [];
 
 // get userInfo from local storage
 // const userInfoFromStorage = localStorage.getItem('userInfo')
 //   ? JSON.parse(localStorage.getItem('userInfo'))
 //   : null;
 
-const preloadedState = {
-  // cart: { cartItems: cartItemsFromStorage },
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
   // userLogin: { userInfo: userInfoFromStorage }
 };
 
 const store = configureStore({
   reducer,
-  preloadedState,
+  initialState,
   devTools: process.env.NODE_ENV !== 'production', //only show devTools when in production
 });
 
