6cc1d790491840fbc4084258b104367cd9ef0fc4
Stephen Moore <judo2000@comcast.net>
2022-07-01 11:28:25 -0600

    Fix bug to clear userlist when admin logs out

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { listUsers } from '../actions/userActions';
+import { useNavigate } from 'react-router-dom';
 
 const UserListScreen = () => {
   const dispatch = useDispatch();
@@ -12,9 +13,18 @@ const UserListScreen = () => {
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const navigate = useNavigate();
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      navigate('/login');
+    }
+  }, [dispatch, userInfo, navigate]);
 
   const deleteHandler = () => {
     console.log('Delete user');
