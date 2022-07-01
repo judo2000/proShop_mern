01435698f832be1e1208d028b56956809ddcac33
Stephen Moore <judo2000@comcast.net>
2022-07-01 07:24:52 -0600

    Create user and order details reset on logout

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -24,7 +24,7 @@ const LoginScreen = ({ location }) => {
   const navigate = useNavigate();
   useEffect(() => {
     if (userInfo) {
-      navigate(`/${redirect}`);
+      navigate(`/`);
     }
   }, [navigate, userInfo, redirect]);
 
